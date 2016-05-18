94c3f0100515a204146142893bb9e5eaf574971d
kristaps <>
2016-05-18 17:02:47 +0000

    Document goop and have both Linux and Mac OS X define setresuid et al.

--- config.h
+++ config.h
@@ -1,11 +1,25 @@
+/*
+ * Require libbsd's stdlib.h for arc4random() etc.
+ * Require <grp.h> for setgroups().
+ */
 #ifdef __linux__
 # define _GNU_SOURCE
 # include <bsd/stdlib.h>
 # include <grp.h>
+#endif
+
+/*
+ * Neither Linux nor Apple have this.
+ */
+#if defined(__linux__) || defined(__APPLE__)
+# include <unistd.h>
 int	setresgid(gid_t, gid_t, gid_t);
 int	setresuid(gid_t, gid_t, gid_t);
 #endif
 
+/*
+ * FreeBSD goop.
+ */
 #ifdef __FreeBSD__
 #include <sys/socket.h>
 #include <sys/types.h>
