2d209d45f468d8d48f5c2d770fffc99306bb91cc
kristaps <>
2016-11-01 02:08:17 +0000

    Sync.

--- revokeproc.c
+++ revokeproc.c
@@ -100,7 +100,7 @@ X509expires(X509 *x)
 
 int
 revokeproc(int fd, const char *certdir, int force, int revocate,
-	const char *const *alts, size_t altsz)
+	int expand, const char *const *alts, size_t altsz)
 {
 	char		*path = NULL, *der = NULL, *dercp, 
 			*der64 = NULL, *san = NULL, *str, *tok;
@@ -177,8 +177,8 @@ revokeproc(int fd, const char *certdir, int force, int revocate,
 
 	/*
 	 * Next, the long process to make sure that the SAN entries
-	 * listed with the certificate fully cover those passed on the
-	 * comamnd line.
+	 * listed with the certificate cover those passed on the comamnd
+	 * line.
 	 */
 
 	extsz = NULL != x->cert_info->extensions ?
@@ -233,7 +233,10 @@ revokeproc(int fd, const char *certdir, int force, int revocate,
 
 	/*
 	 * Parse the SAN line.
-	 * Make sure that all of the domains are represented only once.
+	 * Make sure that all of the domains are represented by the
+	 * input domains: we don't allowing removing domains from
+	 * certificates.
+	 * While here, also check for duplicates.
 	 */
 
 	str = san;
@@ -260,9 +263,22 @@ revokeproc(int fd, const char *certdir, int force, int revocate,
 		}
 	}
 
+	/*
+	 * Reverse test: make sure that all of our input domains are
+	 * represented in the certificate.
+	 * If we're allowed to expand, however, allow new domains to
+	 * slip by with a warning.
+	 */
+
 	for (j = 0; j < altsz; j++) {
 		if (found[j])
 			continue;
+		if (expand) {
+			dodbg("%s/%s: expanding with domain: %s",
+				certdir, CERT_PEM, alts[j]);
+			force = 1;
+			continue;
+		}
 		warnx("%s/%s: domain not listed: %s",
 			certdir, CERT_PEM, alts[j]);
 		goto out;
